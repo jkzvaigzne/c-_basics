56a16dcbd97585dff9639b60a2169107f37932c5
Jēkabs Andrejs Zvaigzne <47506475+juk-developer@users.noreply.github.com>
2023-12-21 21:32:02 +0200

    Exercise #8 task - remove magic numbers, fix languages and typos.

--- c-sharp-syllabus-main/csharp-basics/exercises/TypesAndVariables/Exercise8/Program.cs
+++ c-sharp-syllabus-main/csharp-basics/exercises/TypesAndVariables/Exercise8/Program.cs
@@ -6,15 +6,21 @@ namespace Exercise8
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("ievadi minutes");
+            Console.WriteLine("Enter minutes");
             var minutes = int.Parse(Console.ReadLine());
 
-            var minutesInYear = 365 * 24 * 60;
-            var miutesInDay = 24 * 60;
+            var daysInYearFullYear = 365;
+            
+            var hoursInDay = 24;
+            var minutesInHour = 60;
+           
+
+            var minutesInYear = daysInYearFullYear * hoursInDay * minutesInHour;
+            var minutesInDay = hoursInDay * minutesInHour;
 
             var years = minutes / minutesInYear;
             var minutesToDays = minutes % minutesInYear;
-            var days = minutesToDays / miutesInDay;
+            var days = minutesToDays / minutesInDay;
             
             Console.WriteLine($"it's {years} years and {days} days");
 
