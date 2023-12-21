72c4985384dbbbc76ac16403b4c6070e8ef25106
Jēkabs Andrejs Zvaigzne <47506475+juk-developer@users.noreply.github.com>
2023-12-21 20:56:27 +0200

    Exercise #7 task

---
+++ c-sharp-syllabus-main/csharp-basics/exercises/TypesAndVariables/Exercise7/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Exercise7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ievadi vardu");
+            string userChoice = Console.ReadLine();
+
+            int counter = 0;
+            foreach (char bigLetters in userChoice)
+            {
+                if (char.IsUpper(bigLetters))
+                {
+                    counter++;
+                }
+            }
+
+            Console.WriteLine($"Lielie burti vardā: {counter}");
+            Console.ReadLine();
+        }
+    }
+}
