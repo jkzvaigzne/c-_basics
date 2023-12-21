7ae49f6562f23898da825994f528908806206e7f
Jēkabs Andrejs Zvaigzne <47506475+juk-developer@users.noreply.github.com>
2023-12-21 11:48:24 +0200

    Tasks Exercise 4

---
+++ c-sharp-syllabus-main/csharp-basics/exercises/TypesAndVariables/Exercise4/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Exercise4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Jēkabs";
+            int yearOfBirth = 1995;
+
+            Console.WriteLine("My name is " + name + " and I was born in " + yearOfBirth + ".");
+            Console.Write($"My name is {name} and I was born in {yearOfBirth}.");
+
+            Console.ReadKey();
+        }
+    }
+}
