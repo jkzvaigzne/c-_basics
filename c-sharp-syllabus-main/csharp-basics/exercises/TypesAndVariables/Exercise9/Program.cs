3b48dcd67c79f646e12e7bea009ee14c14f81c50
Jēkabs Andrejs Zvaigzne <47506475+juk-developer@users.noreply.github.com>
2023-12-22 20:15:27 +0200

    Exercise #9 task

---
+++ c-sharp-syllabus-main/csharp-basics/exercises/TypesAndVariables/Exercise9/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Exercise9
+{
+    using System;
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Ievadi distanci metros:");
+            double distanceMeters = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Ievadi laiku stundās:");
+            int hours = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Ievadi laiku minutēs:");
+            int minutes = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Ievadi laiku sekundēs:");
+            int seconds = Convert.ToInt32(Console.ReadLine());
+
+            int totalTimeInSeconds = hours * 3600 + minutes * 60 + seconds;
+
+            double speedMetersPerSecond = distanceMeters / totalTimeInSeconds;
+            double speedKilometersPerHour = (distanceMeters / 1000) / (totalTimeInSeconds / 3600.0);
+            double speedMilesPerHour = (distanceMeters / 1609) / (totalTimeInSeconds / 3600.0);
+
+            Console.WriteLine($"Speed: {speedMetersPerSecond:F2} m/s");
+            Console.WriteLine($"Speed: {speedKilometersPerHour:F2} km/h");
+            Console.WriteLine($"Speed: {speedMilesPerHour:F2} mph");
+
+            Console.Read();
+        }
+    }
+}
