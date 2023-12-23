1c7b0a5da4018aeb93d063be1db9ce6a2c9d245b
Jēkabs Andrejs Zvaigzne <47506475+juk-developer@users.noreply.github.com>
2023-12-23 13:17:59 +0200

    Exercise #2 - odd ||  even

---
+++ c-sharp-syllabus-main/csharp-basics/exercises/Arithmetic/Exercise_#2/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Exercise__2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pāris vai nepāris");
+            Console.WriteLine("Ievadi ciparu:");
+
+            int userNumber;
+
+            while(!int.TryParse(Console.ReadLine(), out userNumber))
+            {
+                Console.WriteLine("To ko? Programma pieņem tikai skaitļus!");
+            }
+
+            string result = (userNumber % 2 == 0) ? "Tu ievadīji pāra skaitli" : "Tu ievadīji nepāra skaitli";
+            Console.WriteLine(result);
+
+            Console.WriteLine("Bye!");
+            Console.Read();
+        }
+    }
+}
