77543a7069cc19df93d4a5f2fd5e219aa66437be
Jēkabs Andrejs Zvaigzne <47506475+juk-developer@users.noreply.github.com>
2023-12-23 12:39:54 +0200

    Exercise #1 - specific conditions.

---
+++ c-sharp-syllabus-main/csharp-basics/exercises/Arithmetic/Exercise_#1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Exercise__1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ievadi pirmo skaitli: ");
+            int userChoiceFirst = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Tu ievadīji: {userChoiceFirst}");
+
+            Console.Write("Ievadi otro skaitli: ");
+            int userChoiceSecond = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Tu ievadīji: {userChoiceSecond}");
+
+            bool checkConditions = CheckConditions(userChoiceFirst, userChoiceSecond);
+
+            Console.WriteLine($"Rezultāts: {checkConditions}");
+            Console.Read();
+        }
+
+        static bool CheckConditions(int numberOne, int numberTwo)
+        {
+            return (numberOne == 15 || numberTwo == 15 || numberOne + numberTwo == 15 || numberOne * numberTwo == 15);
+        }
+    }
+}
