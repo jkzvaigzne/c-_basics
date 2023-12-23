746b0d68c7e6b940e506b181e1d4f1342a4c0f31
Jēkabs Andrejs Zvaigzne <47506475+juk-developer@users.noreply.github.com>
2023-12-23 15:09:41 +0200

    Exercise #4 - 1×2×3×...×10

---
+++ c-sharp-syllabus-main/csharp-basics/exercises/Arithmetic/Exercise_#4/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Exercise__4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = CalculateProduct(1, 10);
+            Console.WriteLine("Rezultātu summa no: 1×2×3×4x5x6x7x8x9x10");
+            Console.WriteLine($"Summa ir: {result}");
+            Console.Read();
+        }
+
+        static int CalculateProduct(int start, int end) 
+        {
+            int productBase = 1;
+
+            for(int i = start; i <= end; i++)
+            {
+                productBase *= i;
+            }
+
+            return productBase;
+        }
+    }
+}
