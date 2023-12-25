e1673727f959ee3d2ad679970d4731560e53a5a7
Jēkabs Andrejs Zvaigzne <47506475+juk-developer@users.noreply.github.com>
2023-12-25 19:23:08 +0200

    Exercise_#9 - BMI calculator

---
+++ c-sharp-syllabus-main/csharp-basics/exercises/Arithmetic/Exercise_#9/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Exercise_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BMI();
+            Console.ReadKey();
+        }
+
+        static void BMI()
+        {
+            Console.WriteLine("Body Mass Index - Calculator");
+            Console.WriteLine("Enter your body weight kilograms: ");
+            double weight;
+
+            if (!double.TryParse(Console.ReadLine(), out weight))
+            {
+                Console.WriteLine("Calculator accepts only numbers. Enter weight.");
+                return;
+            }
+
+            Console.WriteLine("Enter your body height in meters: ");
+            double height;
+
+            if (!double.TryParse(Console.ReadLine(), out height))
+            {
+                Console.WriteLine("Calculator accepts only numbers. Enter height.");
+                return;
+            }
+
+            double bmi = Calc(weight, height);
+            Console.WriteLine($"Your BMI is: {bmi:F2}");
+
+            string result = Check(bmi);
+            Console.WriteLine($"You are {result}");
+        }
+
+        static double Calc(double weight, double height)
+        {
+            return weight / (height * height);
+        }
+
+        static string Check(double bodyMassIndex)
+        {
+            return bodyMassIndex < 18.5 ? "underweight" : (bodyMassIndex >= 18.5 && bodyMassIndex <= 25) ? "optimal weight" : "overweight";
+        }
+    }
+}
