05f06c2e3f48edb1b390978010a26ceb066e6fd3
Jēkabs Andrejs Zvaigzne <47506475+juk-developer@users.noreply.github.com>
2023-12-25 13:44:48 +0200

    Exercise#6-CozaLozaWoza

---
+++ c-sharp-syllabus-main/csharp-basics/exercises/Arithmetic/Exercise_#6/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Exercise__6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CozaLozaWoza();
+            Console.Read();
+        }
+
+        static void CozaLozaWoza()
+        {
+            int smallestNum = 1;
+            int biggestNum = 110;
+            int numInLine = 11;
+
+            for(int i = smallestNum; i <= biggestNum; i++)
+            {
+                string result =
+                    (i % 3 == 0) ? "Coza" :
+                    (i % 5 == 0) ? "Loza" :
+                    (i % 7 == 0) ? "Woza" : i.ToString();
+                
+            Console.WriteLine(result + " ");
+
+                if(i % numInLine == 0)
+                {
+                    Console.WriteLine();
+                }
+            }
+
+        }
+    }
+}
