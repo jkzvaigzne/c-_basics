1c2712d9c8eba3588e99067aaa8aa386b4561c4e
Jēkabs Andrejs Zvaigzne <47506475+juk-developer@users.noreply.github.com>
2023-12-25 15:14:26 +0200

    Exercise #8-CalculateSalary

---
+++ c-sharp-syllabus-main/csharp-basics/exercises/Arithmetic/Exercise_#8/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Exercise__8
+{
+    internal class Program
+    {
+        static void CalculateSalary(double baseSallary, int workedHours, string worker)
+        {
+            const double minWage = 8.00;
+            const int workWeekHours = 40;
+            const int maxWorkHours = 60;
+
+            double totalSallary =
+                (baseSallary < minWage) ? 0 :
+                (workedHours > maxWorkHours) ? 0 :
+                (workedHours <= workWeekHours) ? workedHours * baseSallary :
+                workWeekHours * baseSallary + (workedHours - workWeekHours) * baseSallary * 1.5;
+
+            string errorMsg = (baseSallary < minWage) ?
+                "Error:Base pay less than the min wage" :
+                (workedHours > maxWorkHours) ? "Error:Work hours worked exceed the max limit." : "";
+
+            string outputMessage = $"{worker}: {(errorMsg != "" ? errorMsg : $"Sallary: ${totalSallary:F2}")}";
+            Console.WriteLine(outputMessage);
+        }
+
+        static void Main(string[] args)
+        {
+            CalculateSalary(6.20, 35, "Janis");
+            CalculateSalary(12.00, 73, "Elizabete");
+            CalculateSalary(9.30, 47, "Peteris");
+
+            Console.Read();
+        }
+    }
+}
