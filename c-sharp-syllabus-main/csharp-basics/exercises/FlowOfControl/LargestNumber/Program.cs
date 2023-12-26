2d4b47ef42c2b49f179af101dbd4b174092343e1
Jēkabs Andrejs Zvaigzne <47506475+juk-developer@users.noreply.github.com>
2023-12-26 16:12:44 +0200

    Exercise #1 - LargetsNumber

--- c-sharp-syllabus-main/csharp-basics/exercises/FlowOfControl/LargestNumber/Program.cs
+++ c-sharp-syllabus-main/csharp-basics/exercises/FlowOfControl/LargestNumber/Program.cs
@@ -4,24 +4,34 @@ namespace LargestNumber
 {
     class Program
     {
-        //TODO: Write a C# program to to find the largest of three numbers.
+
+        static int FindLargestNumber(int numberOne, int numberTwo, int numberThree)
+        {
+            return (numberOne > numberTwo) ? ((numberOne > numberThree) ? numberOne : numberThree) : ((numberTwo > numberThree) ? numberTwo : numberThree);
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Input the 1st number: ");
-            var input1 = Console.ReadLine();
+            var firstUserInput = Console.ReadLine();
+            int numberOne = Convert.ToInt32(firstUserInput);
 
             Console.WriteLine("Input the 2nd number: ");
-            var input2 = Console.ReadLine();
+            var secondUserInput = Console.ReadLine();
+            int numberTwo = Convert.ToInt32(secondUserInput);
 
             Console.WriteLine("Input the 3rd number: ");
-            var input3 = Console.ReadLine();
-        
-            /*
-            todo - expected output:
-            Input the 1st number: 25
-            Input the 2nd number: 78
-            Input the 3rd number: 87
-             */
+            var thirdUserInput = Console.ReadLine();
+            int numberThree = Convert.ToInt32(thirdUserInput);
+
+            Console.WriteLine($"Input the 1st number: {numberOne}");
+            Console.WriteLine($"Input the 2nd number: {numberTwo}");
+            Console.WriteLine($"Input the 3rd number: {numberThree}");
+
+            int largestNum = FindLargestNumber(numberOne, numberTwo, numberThree);
+            Console.WriteLine($"Largest number is {largestNum}");
+
+            Console.Read();
         }
     }
 }
