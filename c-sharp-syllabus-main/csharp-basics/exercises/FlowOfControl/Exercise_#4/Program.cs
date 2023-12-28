0d839da885bb831074b031aee6bc2265091c8c3c
Jēkabs Andrejs Zvaigzne <47506475+juk-developer@users.noreply.github.com>
2023-12-28 12:18:17 +0200

    Exercise #4 - PrintDays

---
+++ c-sharp-syllabus-main/csharp-basics/exercises/FlowOfControl/Exercise_#4/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Exercise__4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int weekDay = 1;
+            PrintDay(weekDay);
+
+            Console.Read();
+        }
+
+        static void PrintDay(int dayNumber)
+        {
+            switch(dayNumber)
+            {
+                case 0:
+                    Console.WriteLine("Sunday");
+                    break;
+                case 1:
+                    Console.WriteLine("Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("Saturday");
+                    break;
+                default:
+                    Console.WriteLine("Not a valid day");
+                    break;
+            }
+        }
+    }
+}
