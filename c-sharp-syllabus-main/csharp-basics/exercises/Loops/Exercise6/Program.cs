0e0ea54aaa30fd171c216c3bded9b5b54f97d128
Jēkabs Andrejs Zvaigzne <47506475+juk-developer@users.noreply.github.com>
2024-01-03 13:21:12 +0200

    Exercise #6 - Updated FizzBuzz

---
+++ c-sharp-syllabus-main/csharp-basics/exercises/Loops/Exercise6/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Exercise6
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int maxValue;
+            bool isValidInput = false;
+
+            do
+            {
+                Console.Write("Enter positive number:");
+                if (int.TryParse(Console.ReadLine(), out maxValue) && maxValue > 0)
+                {
+                    isValidInput = true;
+                }
+                else
+                {
+                    Console.WriteLine("Please enter positive integer.");
+                }
+            } while (!isValidInput);
+
+            OutputFizzBuzz(maxValue);
+            Console.ReadKey();
+        }
+
+        static void OutputFizzBuzz(int maxValue)
+        {
+            for (int i = 1; i <= maxValue; i++)
+            {
+                string result = (i % 3 == 0 && i % 5 == 0) ? "FizzBuzz" :
+                                (i % 3 == 0) ? "Fizz" :
+                                (i % 5 == 0) ? "Buzz" : i.ToString();
+
+                Console.Write(result + ((i % 20 == 0) ? "\n" : " "));
+            }
+        }
+    }
+}
