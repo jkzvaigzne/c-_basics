b259b0002a32c7ca15f1a951c446760d23f21f73
Jēkabs Andrejs Zvaigzne <47506475+juk-developer@users.noreply.github.com>
2024-01-03 12:46:27 +0200

    Exercise #5 - DisplayWordsAndDots

---
+++ c-sharp-syllabus-main/csharp-basics/exercises/Loops/Exercise5/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Exercise5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first word:");
+            string firstUserInput = Console.ReadLine();
+
+            Console.WriteLine("Enter second word:");
+            string secondUserInput = Console.ReadLine();
+
+            DisplayWordsAndDots(firstUserInput, secondUserInput);
+
+            Console.Read();
+        }
+
+        static void DisplayWordsAndDots(string firstWord, string secondWord)
+        {
+            int lineHeigth = 30;
+            int countDots = lineHeigth - (firstWord.Length + secondWord.Length);
+
+            Console.WriteLine(firstWord);
+            
+            printDots(countDots);
+
+            Console.WriteLine(secondWord);
+        }
+
+        static void printDots(int counter)
+        {
+            for(int i = 0; i < counter; i++)
+            {
+                Console.WriteLine(".");
+            }
+        }
+    }
+}
