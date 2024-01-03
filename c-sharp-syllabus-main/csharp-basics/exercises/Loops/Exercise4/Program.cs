1b6dd9ec90685fe8eae4d44eda478ec3761edaa9
Jēkabs Andrejs Zvaigzne <47506475+juk-developer@users.noreply.github.com>
2024-01-03 12:26:02 +0200

    Exercise #4 - PrintVowels

--- c-sharp-syllabus-main/csharp-basics/exercises/Loops/Exercise4/Program.cs
+++ c-sharp-syllabus-main/csharp-basics/exercises/Loops/Exercise4/Program.cs
@@ -1,16 +1,34 @@
-﻿namespace Exercise4
+﻿using System;
+
+namespace Exercise4
 {
     class Program
     {
-        //TODO: print all vowels using for and foreach
         static void Main(string[] args)
         {
             char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
 
-            //todo - use for
+            printVowelsForLoop(vowels);
+            Console.WriteLine();
+            printVowelsForeachLoop(vowels);
 
-            //todo - use foreach
+            Console.Read();
+        }
 
+        static void printVowelsForLoop(char[] vowels)
+        {
+            for(int i = 0; i < vowels.Length; i++)
+            {
+                Console.WriteLine(vowels[i]);
+            }
+        }
+
+        static void printVowelsForeachLoop(char[] vowels)
+        {
+            foreach(char character in vowels)
+            {
+                Console.WriteLine(character);
+            }
         }
     }
 }
