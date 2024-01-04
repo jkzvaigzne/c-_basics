2750858c0bcee42f8ee5cc171713aad603d9a9dc
Jēkabs Andrejs Zvaigzne <47506475+juk-developer@users.noreply.github.com>
2024-01-04 17:37:13 +0200

    Exercise # 9 - RollDie and get Desired sum

---
+++ c-sharp-syllabus-main/csharp-basics/exercises/Loops/Exercise9/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class RollTwoDice
+{
+    static void Main()
+    {
+        Console.WriteLine("Desired sum");
+
+        Random random = new Random();
+
+        int sum;
+        do
+        {
+            Console.Write("Enter number: ");
+        } while (!int.TryParse(Console.ReadLine(), out sum));
+
+        RollDie(random, sum);
+
+        Console.ReadLine();
+    }
+
+    static void RollDie(Random random, int desiredSum)
+    {
+        while (true)
+        {
+            int rollOne = random.Next(1, 6) + 1;
+            int rollTwo = random.Next(1, 6) + 1;
+            int result = rollOne + rollTwo;
+
+            Console.WriteLine($"{rollOne} and {rollTwo} = {result}");
+
+            if (result == desiredSum)
+            {
+                break;
+            }
+        }
+    }
+}
