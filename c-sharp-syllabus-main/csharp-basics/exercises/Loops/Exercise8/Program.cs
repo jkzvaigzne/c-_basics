57bed23765418ab1352b15fc7481da48c8437163
Jēkabs Andrejs Zvaigzne <47506475+juk-developer@users.noreply.github.com>
2024-01-04 16:58:07 +0200

    Exercise #8 - DrawFigure

---
+++ c-sharp-syllabus-main/csharp-basics/exercises/Loops/Exercise8/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class AsciiFigure
+{
+    const int Size = 3;
+
+    static void Main()
+    {
+        char slashChar = '/';
+        char asteriskChar = '*';
+        char backslashChar = '\\';
+
+        DrawFigure(Size, slashChar, asteriskChar, backslashChar);
+
+        Console.Read();
+    }
+
+    static void DrawFigure(int size, char slashes, char stars, char backslashes)
+    {
+        int i = 0;
+        while (i < size)
+        {
+            int j = 0;
+
+            while (j++ < 2 * (size - i)) Console.Write(slashes);
+            j = 0;
+
+            while (j++ < 2 * i) Console.Write(stars);
+            j = 0;
+
+            while (j++ < 2 * (size - i)) Console.Write(backslashes);
+
+            Console.WriteLine();
+            i++;
+        }
+    }
+}
