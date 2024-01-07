cb14f98a0232bb8797d4fabd3498d7b80a591ae2
Jēkabs Andrejs Zvaigzne <47506475+juk-developer@users.noreply.github.com>
2024-01-07 15:06:18 +0200

    Exercise #8 - SwapPoints

---
+++ c-sharp-syllabus-main/csharp-basics/exercises/ClassesAndObjects/Exercise8/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Point
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+
+    public Point(int x, int y) => (X, Y) = (x, y);
+}
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Point p1 = new Point(5, 3);
+        Point p2 = new Point(-3, 6);
+
+        (p1.X, p1.Y, p2.X, p2.Y) = (p2.X, p2.Y, p1.X, 2);
+
+        Console.WriteLine($"({p1.X},{p1.Y})");
+        Console.WriteLine($"({p2.X},{p2.Y})");
+
+        Console.Read();
+    }
+}
