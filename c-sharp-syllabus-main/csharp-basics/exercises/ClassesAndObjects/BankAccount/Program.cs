ceb772322d6c977975ac2a692032d9b8ff965c3a
Jēkabs Andrejs Zvaigzne <47506475+juk-developer@users.noreply.github.com>
2024-01-07 16:50:37 +0200

    Exercise #9 - BankAccount

--- c-sharp-syllabus-main/csharp-basics/exercises/ClassesAndObjects/BankAccount/Program.cs
+++ c-sharp-syllabus-main/csharp-basics/exercises/ClassesAndObjects/BankAccount/Program.cs
@@ -1,9 +1,39 @@
-﻿namespace BankAccount
+﻿using System;
+
+namespace BankAccount
 {
+    public class BankAccount
+    {
+        private string _name;
+        private decimal _balance;
+
+        public BankAccount(string name, decimal balance)
+        {
+            _name = name;
+            _balance = balance;
+        }
+
+        public string ShowUserNameAndBalance()
+        {
+            if (_balance >= 0)
+            {
+                return $"{_name}, ${_balance:F2}";
+            }
+            else
+            {
+                return $"{_name}, -${Math.Abs(_balance):F2}";
+            }
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            BankAccount BankAccount = new BankAccount("John Doe", -99.99m);
+
+            Console.WriteLine(BankAccount.ShowUserNameAndBalance());
+            Console.Read();
         }
     }
 }
