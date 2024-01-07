80d857cfefbfc18582705e33ff230b8aef690e3b
Jēkabs Andrejs Zvaigzne <47506475+juk-developer@users.noreply.github.com>
2024-01-07 12:18:36 +0200

    Exercise # 3 FuelGauge & Odometer

---
+++ c-sharp-syllabus-main/csharp-basics/exercises/ClassesAndObjects/Exercise3/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Exercise3
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var fuelGauge = new FuelGauge(GetValidInt("Enter fuel: "));
+            var odometer = new Odometer(GetValidInt("Enter odometer stats: "), fuelGauge);
+
+            while (true)
+            {
+                if (GetValidUserYesOrNo("Do you want to drive +10km? (y or n)") != 'y')
+                    break;
+
+                for (var i = 0; i < 10; i++)
+                    odometer.IncrementMileage();
+
+                Console.WriteLine($"Current mileage: {odometer.GetMileage()} Fuel: {fuelGauge.GetFuelAmount()}");
+
+                if (fuelGauge.GetFuelAmount() <= 0)
+                {
+                    Console.WriteLine($"Your tank is empty and you travel {odometer.GetMileage()} km.");
+                    break;
+                }
+            }
+
+            Console.ReadLine();
+        }
+
+        private static int GetValidInt(string message)
+        {
+            int result;
+            while (true)
+            {
+                Console.WriteLine(message);
+                if (int.TryParse(Console.ReadLine(), out result) && result >= 0)
+                    return result;
+            }
+        }
+
+        private static char GetValidUserYesOrNo(string message)
+        {
+            char userChoice;
+            Console.Write(message);
+            while (true)
+            {
+                userChoice = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+                if (userChoice == 'y' || userChoice == 'n')
+                    return userChoice;
+            }
+        }
+    }
+}
