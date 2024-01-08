d4fbb72776ce6312181eefe7e251a4c0f6d607bb
Jēkabs Andrejs Zvaigzne <47506475+juk-developer@users.noreply.github.com>
2024-01-08 12:59:44 +0200

    Exercise # 11 - BankAccount

--- c-sharp-syllabus-main/csharp-basics/exercises/ClassesAndObjects/Account/Account.cs
+++ c-sharp-syllabus-main/csharp-basics/exercises/ClassesAndObjects/Account/Account.cs
@@ -1,7 +1,33 @@
 ﻿namespace Account
 {
-    class Account
+    public class Account
     {
-        
+        private int _id;
+        private string _accName;
+        private decimal _accBalance;
+
+        public Account(string accName, decimal accBalance, int id)
+        {
+            _id = id;
+            _accName = accName;
+            _accBalance = accBalance;
+        }
+
+        public void AccountWithdrawal(decimal amount) => _accBalance -= amount;
+
+        public void AccountDeposit(decimal amount) => _accBalance += amount;
+
+        public string AccountName() => _accName;
+
+        public decimal AccountBalance() => _accBalance;
+
+        public static class Bank
+        {
+            public static void Transfer(Account source, Account destination, decimal howMuch)
+            {
+                source.AccountWithdrawal(howMuch);
+                destination.AccountDeposit(howMuch);
+            }
+        }
     }
 }
