c5fdf7f9dfc8b5cf8f8dd7b940df401d5d26aa15
Jēkabs Andrejs Zvaigzne <47506475+juk-developer@users.noreply.github.com>
2024-01-09 16:56:27 +0200

    List Exercise #3 - Insert color

--- c-sharp-syllabus-main/csharp-basics/exercises/Collections/ListExercise3/Program.cs
+++ c-sharp-syllabus-main/csharp-basics/exercises/Collections/ListExercise3/Program.cs
@@ -7,8 +7,6 @@ namespace ListExercise3
     {
         static void Main(string[] args)
         {
-            //TODO: Write a C# program to insert an element into the array list at the first position.
-
             var colors = new List<string>();
             colors.Add("Red");
             colors.Add("Green");
@@ -16,12 +14,14 @@ namespace ListExercise3
             colors.Add("White");
             colors.Add("Black");
 
+            colors.Insert(0, "Forest Green");
             Console.WriteLine(string.Join(",", colors));
 
-            //TODO: Add new color at the first and third position of the list
-
-            //Print the list
+            colors.Insert(0, "Light Coral");
+            colors.Insert(2, "Blue Violet");
             Console.WriteLine(string.Join(",", colors));
+
+            Console.Read();
         }
     }
 }
