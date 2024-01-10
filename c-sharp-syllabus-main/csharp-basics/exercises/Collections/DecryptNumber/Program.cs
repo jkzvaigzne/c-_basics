c9c774aa880585e6907bb52572debd5ae8cf7a3c
Jēkabs Andrejs Zvaigzne <47506475+juk-developer@users.noreply.github.com>
2024-01-10 14:15:42 +0200

    Exercise - CryptedNums

--- c-sharp-syllabus-main/csharp-basics/exercises/Collections/DecryptNumber/Program.cs
+++ c-sharp-syllabus-main/csharp-basics/exercises/Collections/DecryptNumber/Program.cs
@@ -1,14 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace DecryptNumber
 {
     internal class Program
     {
-        // look at the keyboard.
         static void Main(string[] args)
         {
-            var cryptedNumbers = new List<string>
+            var cryptedNums = new List<string>
             {
                 "())(",
                 "*$(#&",
@@ -17,6 +17,27 @@ namespace DecryptNumber
                 "!)(^&(#@",
                 "!)(#&%(*@#%"
             };
+
+            string symbols = ")!@#$%^&*(";
+
+            var key = new Dictionary<char, char>();
+
+            for (int i = 0; i < symbols.Length; i++)
+            {
+                key[symbols[i]] = (i + 1).ToString()[0];
+            }
+
+            var nums = cryptedNums.Select(crypted =>
+                string.Concat(crypted.Select(c => key.ContainsKey(c) ? key[c] : c))
+            ).ToList();
+
+
+            foreach (var num in nums)
+            {
+                Console.WriteLine($"Numbers: {num}");
+            }
+
+            Console.Read();
         }
     }
 }
