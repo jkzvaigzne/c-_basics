c236e5b8efbcc45615de16eb37c05c622bf4838f
Jēkabs Andrejs Zvaigzne <47506475+juk-developer@users.noreply.github.com>
2024-01-11 11:10:34 +0200

    Exercise #4 - unique names

---
+++ c-sharp-syllabus-main/csharp-basics/exercises/Collections/Exercise4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercise4
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var userChoiceNames = new HashSet<string>();
+
+            while (ReadUserNames(userChoiceNames)) { }
+
+            Console.Write("\n");
+            Console.WriteLine($"unique name list of names: {string.Join(",", userChoiceNames)}");
+            
+            Console.Read();
+        }
+
+        static bool ReadUserNames(HashSet<string> names)
+        {
+            Console.Write("enter name:");
+            string userChoice = Console.ReadLine()?.Trim().ToString();
+
+            return string.IsNullOrWhiteSpace(userChoice) ? false : names.Add(userChoice);
+        }
+    }
+}
