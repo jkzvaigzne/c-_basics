942df35a645c83ff52f7d3be5764fc70b781eb65
Jēkabs Andrejs Zvaigzne <47506475+juk-developer@users.noreply.github.com>
2024-01-12 13:49:17 +0200

    Exercise #9 - PhoneBook

--- c-sharp-syllabus-main/csharp-basics/exercises/Collections/Phonebook/Program.cs
+++ c-sharp-syllabus-main/csharp-basics/exercises/Collections/Phonebook/Program.cs
@@ -1,9 +1,19 @@
-﻿namespace Phonebook
+﻿using System;
+using PhoneBook;
+
+namespace Phonebook
 {
     class Program
     {
         static void Main(string[] args)
         {
+            PhoneDirectory phoneDirectory = new PhoneDirectory();
+
+            phoneDirectory.PutNumber("John Doe", "371-288-888-88");
+
+            Console.WriteLine($"John Doe: {phoneDirectory.GetNumber("John Doe")}");
+
+            Console.Read();
         }
     }
 }
